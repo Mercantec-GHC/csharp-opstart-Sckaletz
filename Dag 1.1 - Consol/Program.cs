65f29146c23e6b67158c0a5cfc642b46d95ab78f
Sckaletz <allan@leeloo.dk>
2024-02-19 12:04:27 +0100

    Part update

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -2,7 +2,9 @@
 {
     private static void Main(string[] args)
     {
+        // ------------------------------------------------------------------------------------------
         // Write your first C# code
+        // ------------------------------------------------------------------------------------------
         /*
         Console.Write("This ");
         Console.Write("is the first ");
@@ -10,7 +12,9 @@
         Console.WriteLine("This is the second line.");
         */
 
+        // ------------------------------------------------------------------------------------------
         // Store and retrieve data using literal and variable values in C#
+        // ------------------------------------------------------------------------------------------
         /*
         Console.WriteLine('b');
         Console.WriteLine(123);
@@ -45,7 +49,9 @@
         Console.WriteLine("Hello, " + firstName + "! You have " + number + " messages in your inbox. The temperature is " + number2 + " celsius.");
         */
 
+        // ------------------------------------------------------------------------------------------
         // Perform basic string formatting in C#
+        // ------------------------------------------------------------------------------------------
         /*
         Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
         Console.WriteLine("Invoice: 1021\t\tComplete!");
@@ -83,6 +89,7 @@
         Console.WriteLine($@"C:\Output\{projectName}\Data");
         */
 
+        /*
         string projectName = "ACME";
         string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
         Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
@@ -90,5 +97,91 @@
         string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
         string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
         Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
+        */
+
+        // ------------------------------------------------------------------------------------------
+        // Perform basic operations on numbers in C#
+        // ------------------------------------------------------------------------------------------
+        /*
+        int firstNumber = 12;
+        int secondNumber = 7;
+        Console.WriteLine(firstNumber + secondNumber);
+
+        string firstName = "Bob";
+        int widgetsSold = 7;
+        Console.WriteLine(firstName + " sold " + widgetsSold + " widgets.");
+        */
+
+        /*
+        string firstName = "Bob";
+        int widgetsSold = 7;
+        Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+        */
+
+        /*
+        int sum = 7 + 5;
+        int difference = 7 - 5;
+        int product = 7 * 5;
+        int quotient = 7 / 5;
+
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Difference: " + difference);
+        Console.WriteLine("Product: " + product);
+        Console.WriteLine("Quotient: " + quotient);
+
+        decimal decimalQuotient = 7.0m / 5;
+        Console.WriteLine($"Decimal quotient: {decimalQuotient}");
+        */
+
+        /*
+        int first = 7;
+        int second = 5;
+        decimal quotient = (decimal)first / (decimal)second;
+        Console.WriteLine(quotient);
+
+        Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+        Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+
+        int value1 = 3 + 4 * 5;
+        int value2 = (3 + 4) * 5;
+        Console.WriteLine(value1);
+        Console.WriteLine(value2);
+        */
+
+        /*
+        int value = 1;
+
+        value = value + 1;
+        Console.WriteLine("First increment: " + value);
+
+        value += 1;
+        Console.WriteLine("Second increment: " + value);
+
+        value++;
+        Console.WriteLine("Third increment: " + value);
+
+        value = value - 1;
+        Console.WriteLine("First decrement: " + value);
+
+        value -= 1;
+        Console.WriteLine("Second decrement: " + value);
+
+        value--;
+        Console.WriteLine("Third decrement: " + value);
+        */
+
+        /*
+        int value = 1;
+        value++;
+        Console.WriteLine("First: " + value);
+        Console.WriteLine($"Second: {value++}");
+        Console.WriteLine("Third: " + value);
+        Console.WriteLine("Fourth: " + (++value));
+        */
+
+        /*
+        int fahrenheit = 94;
+        Console.WriteLine($"The temperature is " + ((fahrenheit - 32m) * (5m/9m)));
+        */
     }
 }
