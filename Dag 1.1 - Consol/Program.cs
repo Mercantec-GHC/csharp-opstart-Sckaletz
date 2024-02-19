e4ad6616004ffc4210963afbc6b4c8817d7e8240
Sckaletz <allan@leeloo.dk>
2024-02-19 09:59:33 +0100

    Opgave 1.1 done

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("This ");
+        Console.Write("is the first ");
+        Console.Write("line.\n");
+        Console.WriteLine("This is the second line.");
+    }
+}
