543d7397863b5485b005a20da2d071e8f7bce38e
Sckaletz <allan@leeloo.dk>
2024-02-19 10:25:09 +0100

    Part 2

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -2,9 +2,46 @@
 {
     private static void Main(string[] args)
     {
+        // Write your first C# code
+        /*
         Console.Write("This ");
         Console.Write("is the first ");
         Console.Write("line.\n");
         Console.WriteLine("This is the second line.");
+        */
+
+        // Store and retrieve data using literal and variable values in C#
+        /*
+        Console.WriteLine('b');
+        Console.WriteLine(123);
+        Console.WriteLine(0.25F);
+        Console.WriteLine(2.625);
+        Console.WriteLine(12.39816m);
+        Console.WriteLine(true);
+        Console.WriteLine(false);
+        */
+
+        /*
+        string firstName;
+        firstName = "Bob";
+        Console.WriteLine(firstName);
+        firstName = "Liem";
+        Console.WriteLine(firstName);
+        firstName = "Isabella";
+        Console.WriteLine(firstName);
+        firstName = "Yasmin";
+        Console.WriteLine(firstName);
+        */
+
+        /*
+        string firstName = "Bob";
+        Console.WriteLine(firstName);
+        */
+
+        string firstName = "Bob";
+        int number = 3;
+        float number2 = 34.4F;
+        Console.WriteLine("Hello, " + firstName + "! You have " + number + " messages in your inbox. The temperature is " + number2 + " celsius.");
+        
     }
 }
