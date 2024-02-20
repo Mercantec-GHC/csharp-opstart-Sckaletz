01f32ede18ae8dd634756e3ea2134bf4ca91b4b7
Sckaletz <allan@leeloo.dk>
2024-02-20 10:42:43 +0100

    Update

--- Dag 2.1 - ConsolApp/Program.cs
+++ Dag 2.1 - ConsolApp/Program.cs
@@ -1,2 +1,188 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        // ----------------------------------------------------------------------------------
+        // Call methods from the .NET Class Library using C#
+        // ----------------------------------------------------------------------------------
+        /*
+        Random dice = new Random();
+        int roll = dice.Next(1, 7);
+        Console.WriteLine(roll);
+        */
+
+        /*
+        Random dice = new Random();
+        int roll1 = dice.Next();
+        int roll2 = dice.Next(101);
+        int roll3 = dice.Next(50, 101);
+
+        Console.WriteLine($"First roll: {roll1}");
+        Console.WriteLine($"Second roll: {roll2}");
+        Console.WriteLine($"Third roll: {roll3}");
+        */
+
+        /*
+        int firstValue = 500;
+        int secondValue = 600;
+        int largerValue = Math.Max(firstValue, secondValue);
+
+        Console.WriteLine(largerValue);
+        */
+
+
+        // ----------------------------------------------------------------------------------
+        // Add decision logic to your code using 'if', 'else', and 'else if' statements in C#
+        // ----------------------------------------------------------------------------------
+
+        /*
+        Random dice = new Random();
+
+        int roll1 = dice.Next(1, 7);
+        int roll2 = dice.Next(1, 7);
+        int roll3 = dice.Next(1, 7);
+
+        int total = roll1 + roll2 + roll3;
+
+        Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+        if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+        {
+            if ((roll1 == roll2) && (roll2 == roll3))
+            {
+                Console.WriteLine("You rolled triples!  +6 bonus to total!");
+                total += 6;
+            }
+            else
+            {
+                Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+                total += 2;
+            }
+
+            Console.WriteLine($"Your total including the bonus: {total}");
+        }
+
+        if (total >= 16)
+        {
+            Console.WriteLine("You win a new car!");
+        }
+        else if (total >= 10)
+        {
+            Console.WriteLine("You win a new laptop!");
+        }
+        else if (total == 7)
+        {
+            Console.WriteLine("You win a trip for two!");
+        }
+        else
+        {
+            Console.WriteLine("You win a kitten!");
+        }
+        */
+
+        /*
+        string message = "The quick brown fox jumps over the lazy dog.";
+        bool result = message.Contains("dog");
+        Console.WriteLine(result);
+
+        if (message.Contains("fox"))
+        {
+            Console.WriteLine("What does the fox say?");
+        }
+        */
+
+        /*
+        Random random = new Random();
+        int daysUntilExpiration = random.Next(12);
+        int discountPercentage = 0;
+
+        if (daysUntilExpiration == 0)
+        {
+            Console.WriteLine("Your subscription has expired.");
+        }
+        else if (daysUntilExpiration == 1)
+        {
+            Console.WriteLine("Your subscription expires within a day!");
+            discountPercentage = 20;
+        }
+        else if (daysUntilExpiration <= 5)
+        {
+            Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+            discountPercentage = 10;
+        }
+        else if (daysUntilExpiration <= 10)
+        {
+            Console.WriteLine("Your subscription will expire soon. Renew now!");
+        }
+
+        if (discountPercentage > 0)
+        {
+            Console.WriteLine($"Renew now and save {discountPercentage}%.");
+        }
+        */
+
+
+        // ----------------------------------------------------------------------------------
+        // Store and iterate through sequences of data using Arrays and the foreach statement in C#
+        // ----------------------------------------------------------------------------------
+        /*
+        string[] fraudulentOrderIDs = new string[3];
+
+        fraudulentOrderIDs[0] = "A123";
+        fraudulentOrderIDs[1] = "B456";
+        fraudulentOrderIDs[2] = "C789";
+        */
+
+        /*
+        string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+        Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+        Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+        Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+        fraudulentOrderIDs[0] = "F000";
+
+        Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+        Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+        */
+
+        /*
+        string[] names = { "Rowena", "Robin", "Bao" };
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+        */
+
+        /*
+        int[] inventory = { 200, 450, 700, 175, 250 };
+        int sum = 0;
+        int bin = 0;
+        foreach (int items in inventory)
+        {
+            sum += items;
+            bin++;
+            Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+        }
+        Console.WriteLine($"We have {sum} items in inventory.");
+        */
+
+        /*
+        string[] orders = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+        foreach (string order in orders)
+        {
+            if (order.StartsWith("B"))
+            {
+                Console.WriteLine(order);
+            }
+        }
+        */
+
+
+        // ----------------------------------------------------------------------------------
+        // Create readable code with conventions, whitespace, and comments in C#
+        // ----------------------------------------------------------------------------------
+    }
+}
