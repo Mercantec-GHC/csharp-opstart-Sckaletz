43d026ad7f9dd3e5808b0562477baeba0e646084
Sckaletz <allan@leeloo.dk>
2024-02-21 19:48:32 +0100

    Update

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -2,6 +2,11 @@
 {
     private static void Main(string[] args)
     {
+        // -----------------------------------------------------------------------------------
+        // Choose the correct data type in your C# code
+        // -----------------------------------------------------------------------------------
+
+        /*
         Console.WriteLine("Signed integral types:");
 
         Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
@@ -22,5 +27,16 @@
         Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
         Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
         Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+        */
+
+        /*
+        int[] data = new int[3];
+        */
+
+
+        // -----------------------------------------------------------------------------------
+        // Convert data types using casting and conversion techniques in C#
+        // -----------------------------------------------------------------------------------
+
     }
 }
