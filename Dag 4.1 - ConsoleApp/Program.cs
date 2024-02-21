081ef50b796118999f458a5e397d74916fa37e4c
Sckaletz <allan@leeloo.dk>
2024-02-21 21:30:30 +0100

    Update

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -126,5 +126,41 @@
         if (result > 0)
             Console.WriteLine($"Measurement (w/ offset): {50 + result}");
         */
+
+        /*
+        string[] values = { "12,3", "45", "ABC", "11", "DEF" };
+        decimal total = 0m;
+        string message = "";
+        foreach (var value in values)
+        {
+            decimal number;
+            if (decimal.TryParse(value, out number))
+            {
+                total += number;
+            }
+            else
+            {
+                message += value;
+            }
+        }
+        Console.WriteLine($"Message: {message}");
+        Console.WriteLine($"Total: {total}");
+        */
+
+        /*
+        int value1 = 12;
+        decimal value2 = 6.2m;
+        float value3 = 4.3f;
+
+        int result1 = value1 / decimal.ToInt32(value2);
+        // Hint: You need to round the result to nearest integer (don't just truncate)
+        Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+        decimal result2 = value2 / (decimal)value3;
+        Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+        float result3 = value3 / value1;
+        Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+        */
     }
 }
